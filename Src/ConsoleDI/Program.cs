995f58ee47a9086190948149d1e604a0f4056583
Ricardo Pacheco <ricardo.pacheco.roldan@gmail.com>
2023-01-07 11:29:42 -0600

    Maintain scope

--- Src/ConsoleDI/Program.cs
+++ Src/ConsoleDI/Program.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ConsoleDI;
 
-namespace ConsoleDI
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false, false);
+
+var configuration = builder.Build();
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IConfiguration>(configuration)
+    .AddScoped<Application>()
+    .BuildServiceProvider();
+
+using (var scope = serviceProvider.CreateScope())
 {
-    internal static class Program
-    {
-        public static IConfiguration Configuration { get; private set; }
-
-        static void Main(string[] args)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, false);
-
-            Configuration = builder.Build();
-
-            var serviceProvider = new ServiceCollection()
-                .AddSingleton<IConfiguration>(Configuration)
-                .AddScoped<Application>()
-                .BuildServiceProvider();
-
-            var app = serviceProvider.GetService<Application>();
-            app.Run(args);
-        }
-    }
+    var app = scope.ServiceProvider.GetRequiredService<Application>();
+    app.Run(args);
 }
